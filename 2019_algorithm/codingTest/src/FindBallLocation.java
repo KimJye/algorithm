eb4ce22e9c30cd4d654071747704a61159858a42
KimJye <eoeogudgud@naver.com>
2019-01-05 16:16:11 +0900

    FindBallLocation

---
+++ 2019_algorithm/codingTest/src/FindBallLocation.java
@@ -0,0 +1,35 @@
+﻿/*
+ * Date: 2019. 01. 05.
+ * Author: KimJye | https://github.com/KimJye
+ * Solution URL: https://github.com/KimJye/algorithm
+ * Problem URL: https://www.acmicpc.net/problem/1547 
+ * Title : 공 위치 찾기 (문제 응용)
+ * description : 컵 위치는 1,2,3
+                 시작은 1번째 컵에 공이 들어있다.
+                 컵 위치 바뀌는 순서는 "1,3","2,1"
+ */
+public class FindBallLocation{
+    static int solution(String[] find){
+        char value='1';
+
+        for(int i=0; i<find.length; ++i){
+            if(find[i].charAt(0)!=value && find[i].charAt(2)!=value){
+                continue;
+            }else if(find[i].charAt(0)==value || find[i].charAt(2)==value){
+                if(find[i].charAt(0)==value){
+                    value=find[i].charAt(2);
+                }else{
+                    value=find[i].charAt(0);
+                }
+            }
+        }
+        int answer = value - '0';
+        return answer;
+    }
+    public static void main(String[] args){
+        String [] find = new String [2];
+        solution(find);
+    }
+
+}
+
