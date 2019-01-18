6b1ca5f27e044a879efeb14e3a4a18de997950cb
KimJye <eoeogudgud@naver.com>
2019-01-18 18:33:47 +0900

    단계별 문제풀이 - 문자열

---
+++ 2019_algorithm/backjoon/src/string/P11654.java
@@ -0,0 +1,27 @@
+package string;
+
+import java.util.Scanner;
+
+/*
+ * Date: 2019. 01. 18
+ * Author: KimJye | https://github.com/KimJye
+ * Solution URL: https://github.com/KimJye/algorithm
+ * Problem URL : https://www.acmicpc.net/problem/11654
+ * Title : 아스키 코드
+ * description : 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+
+                 입력 : 알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+
+                 출력 : 입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+
+ * solution : 문자열 문제
+
+ */
+public class P11654 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int input = sc.next().charAt(0);
+
+        System.out.println(input);
+    }
+}
